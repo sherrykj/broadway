6c556864fb4fbc9816c1edcaa6d6a5890d79c261
sherrykj <shaharyar_jadoon@mail.ru>
2019-01-19 14:12:53 +0500

    completed broadway app

---
+++ app/models/play.rb
@@ -0,0 +1,9 @@
+class Play < ApplicationRecord
+  belongs_to :user
+  belongs_to :category
+  has_many :reviews
+
+  has_attached_file :play_img, styles: { play_index: "300x300>", play_show: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :play_img, content_type: /\Aimage\/.*\z/
+
+end
